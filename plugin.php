bbc81cfaa03b35d574fda2416fae265d029d681e
Jeremy Felt <jeremy.felt@gmail.com>
2022-02-22 14:42:13 -0800

    Add WP/php version plugin headers

--- plugin.php
+++ plugin.php
@@ -1,11 +1,13 @@
 <?php
 /**
- * Plugin Name: Show/Hide Section
+ * Plugin Name: Show/Hide Section Block
  * Plugin URI: https://github.com/happyprime/show-hide-section
  * Description: Add an interactive heading that enables users to reveal or hide its associated content.
  * Author: Happy Prime
  * Author URI: https://happyprime.co/
  * Version: 1.0.0
+ * Requires at least: 5.9
+ * Requires PHP: 7.2
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
