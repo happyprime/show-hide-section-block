27afcd2bf26d7440ddb1cc5abb4ef7b60a0f46f3
Elaine Shannon <70334653+webelaine@users.noreply.github.com>
2022-03-21 19:27:45 -0500

    Build files

--- build/show-hide-group.asset.php
+++ build/show-hide-group.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '6c6e620a937abcab098074d8dbb45863');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '48a537a2def4593946006fc1db58926e');
