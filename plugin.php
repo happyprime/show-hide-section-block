6726caec895e9077263aa44e1079a824e6de9eee
Jeremy Felt <jeremy.felt@gmail.com>
2022-11-16 18:41:13 -0800

    Update plugin header

--- plugin.php
+++ plugin.php
@@ -1,11 +1,5 @@
 <?php
 /**
- * The main plugin file.
- *
- * @package happy-prime
- */
-
-/**
  * Plugin Name: Show/Hide Section Block
  * Plugin URI: https://github.com/happyprime/show-hide-section
  * Description: Creates accessible summaries that can be toggled to show or hide additional details.
@@ -13,9 +7,11 @@
  * Author URI: https://happyprime.co/
  * Version: 1.1.0
  * Requires at least: 5.9
- * Requires PHP: 7.2
+ * Requires PHP: 7.4
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package show-hide-section
  */
 
 // If this file is called directly, abort.
