99bd8a8319530667ae812f20b510393b99736565
Jeremy Felt <jeremy.felt@gmail.com>
2023-02-22 17:22:10 -0800

    Use a boolean as the default for a boolean typed attribute
    Fixes #47

--- build/show-hide-section.asset.php
+++ build/show-hide-section.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '237054a27855d3b2fb86');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '030eb74a77268f517d77');
