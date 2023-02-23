5fdebf1cd7102e9730a9122bfc2caa5fbca8fe98
Jeremy Felt <jeremy.felt@gmail.com>
2023-02-22 17:25:12 -0800

    Remove unsupported hideLabelFromVision prop on RichText
    Fixes #46

--- build/show-hide-section.asset.php
+++ build/show-hide-section.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '030eb74a77268f517d77');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '27484815c0357f023747');
