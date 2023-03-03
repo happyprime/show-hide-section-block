f7f21dbc2e51cdd0decc21705eb90d701f015f08
Jeremy Felt <jeremy.felt@gmail.com>
2023-03-02 16:44:29 -0800

    Add a variety of block supports options

--- build/show-hide-section.asset.php
+++ build/show-hide-section.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5e72d721a0420e9f8ae5');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '7a24d20ed1e676a3d350');
