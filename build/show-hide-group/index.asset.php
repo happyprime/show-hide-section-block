8fbcb289697256d6fb596ab63811eb800f68ad22
jeremyfelt <jeremy.felt@gmail.com>
2024-07-23 22:09:19 -0700

    Apply custom JS dev tooling, remove wordpress/scripts dependency
    This is a much smaller toolset and may be more of a pain to manage,
    but feels pretty good right now.
    
    We're unable to continue tracking updates on wordpress/scripts until
    we're ready to drop back-compat for WordPress releases earlier than
    6.6, so it seems smart to start switching to our own tooling.

--- build/show-hide-group/index.asset.php
+++ build/show-hide-group/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9821368a8f1930a9690c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '9eadd2a33a88729b380f');
