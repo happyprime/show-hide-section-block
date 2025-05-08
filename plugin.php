102eb19e95e0d2f351258e8c8150e38db85da1d1
jeremyfelt <jeremy.felt@gmail.com>
2025-05-08 09:19:29 -0700

    Bump 3.1.0

--- plugin.php
+++ plugin.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name: Show / Hide Section Block
  * Description: Display an accessible show/hide interface with details and summary elements.
- * Version: 3.0.0
+ * Version: 3.1.0
  * Plugin URI: https://wordpress.org/plugins/show-hide-section-block/
  * Author: Happy Prime
  * Author URI: https://happyprime.co/
- * License:      GPLv2 or later
- * License URI:  https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain: show-hide-section-block
  * Domain Path: /languages
  * Requires at least: 6.4
